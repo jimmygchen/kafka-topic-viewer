9ecb1bb4ed1ca0574a30de3dfe1548eb305beb6b
Jimmy Chen <jchen.tc@gmail.com>
2017-07-21 22:11:41 +1000

    Add patch for kafka-proxy-ws null check

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 'use strict';
 const KafkaProxy = require('kafka-proxy');
+const patchKafkaProxy = require('./kafkaProxyPatch');
 
 let kafkaProxy = new KafkaProxy({
     wsPort: 9999,
     kafka: 'localhost:9092/',
 });
+
+patchKafkaProxy(kafkaProxy);
 
 kafkaProxy.listen();
---
+++ server/kafkaProxyPatch.js
@@ -0,0 +1,22 @@
+export default (kafkaProxy) => {
+  Object.getPrototypeOf(kafkaProxy)._batchMessageHandler = function(messageSet, topicCombo, partition) {
+    // workaround... nokafka onyl allows three params to be sent to message handlers, so we just concat and then unpack
+    let consumerGroup = topicCombo.split('/')[0];
+    let topic = topicCombo.split('/')[1];
+    let batchSize = messageSet.length - 1;
+    this.clients[consumerGroup][topic].received = this.clients[consumerGroup][topic].received + messageSet.length;
+    this.clients[consumerGroup][topic].backlog = this.clients[consumerGroup][topic].received - this.clients[consumerGroup][topic].sent;
+    //debug(`[${consumerGroup} / ${topic}] received offset: ${messageSet[batchSize].offset}`);
+
+    if (this.clients[consumerGroup][topic]) {
+      this.clients[consumerGroup][topic].ws.send(JSON.stringify(
+        messageSet.map((message) => {
+          return {
+            message: message.message.value && message.message.value.toString(),
+            offset: message.offset
+          }
+        })
+      ), (err) => this._handleSendResponse(err, this.clients[consumerGroup][topic], consumerGroup, topic, partition, messageSet[batchSize].offset, messageSet.length));
+    }
+  };
+}
